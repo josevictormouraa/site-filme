3ae7719cb4b59bae0cca8e78d4f0d98d6ff06e65
josevictormouraa@gmail.com <126627287+josevictormouraa@users.noreply.github.com>
2023-12-01 18:50:58 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,11 @@
+const links = document.querySelectorAll('.icones a')
+links.forEach(element => {
+    element.classList.add('ambos')
+})
+
+
+const addi = document.querySelector('main')
+addi.classList.add('container')
+
+const desc = document.querySelector('main div')
+desc.classList.add('descrition')
